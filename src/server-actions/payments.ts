4aab97421fe0cfa6aa2d139e05e898be62ebf09b
Rasel Miah <rasel6041@gmail.com>
2025-01-03 07:45:07 +0100

    Resolved merge conflicts

--- src/server-actions/payments.ts
+++ src/server-actions/payments.ts
@@ -1,5 +1,36 @@
-'use server'
+/* 'use server'
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export const GetStripeClientSecretKey = async ({
+  amount,
+}: {
+  amount: number;
+}) => {
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: "usd",
+      description: "ZirrahHotels - Room Booking Payment",
+    });
+
+    return {
+      success: true,
+      data: paymentIntent.client_secret,
+    };
+  } catch (error: any) {
+    return {
+      success: false,
+      message: error.message,
+    };
+  }
+}; */
+
+
+
+'use server'
+import Stripe from 'stripe'; // Use import instead of require()
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export const GetStripeClientSecretKey = async ({
   amount,
