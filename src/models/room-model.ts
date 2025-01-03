f59be9ab7e53e791240dc6d4f0cff1151cfabc33
Rasel Miah <rasel6041@gmail.com>
2025-01-03 07:41:09 +0100

    Resolved merge conflicts

--- src/models/room-model.ts
+++ src/models/room-model.ts
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+/* import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema(
   {
@@ -41,6 +41,59 @@ const roomSchema = new mongoose.Schema(
   }
 );
 require("./hotel-model");
+if (mongoose.models && mongoose.models["rooms"]) {
+  delete mongoose.models["rooms"];
+}
+
+const RoomModel = mongoose.model("rooms", roomSchema);
+export default RoomModel;
+ */
+
+
+import mongoose from "mongoose";
+import "./hotel-model"; // Replaces the require() call
+
+const roomSchema = new mongoose.Schema(
+  {
+    hotel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "hotels",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    rentPerDay: {
+      type: Number,
+      required: true,
+    },
+    amenities: {
+      type: String,
+      required: true,
+    },
+    bedrooms: {
+      type: Number,
+      required: true,
+    },
+    roomNumber: {
+      type: Number,
+      required: true,
+    },
+    media: {
+      type: [String],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
 if (mongoose.models && mongoose.models["rooms"]) {
   delete mongoose.models["rooms"];
 }
